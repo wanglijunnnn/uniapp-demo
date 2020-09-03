c7cd0f04a0d22bddbb12a3d95f09551e83b8516b
winna <530269081@qq.com>
2020-09-03 11:21:53 +0800

    1

---
+++ pages/user/index.vue
@@ -0,0 +1,35 @@
+<template>
+	        <view>
+	        <view class="edit_tips_bar">
+	            <view class="edit_tips">
+	                <text>个性化演示</text>
+	                <text>内置多种文稿切换效果，让你的演示变得更引人注目</text>
+	            </view>
+	            <view class="edit_tips">
+	                <text>文件导入</text>
+	                <text>支持PPTX/PDF格式的本地文件导入，自动识别转换</text>
+	            </view>
+	            <view class="edit_tips">
+	                <text>快捷操作</text>
+	                <text>提供各种常用快捷键和操作功能，方便快捷</text>
+	            </view>
+	        </view>
+			<view class="index_footer">
+				<image  src="@/static/uni.png"></image>
+				<text>1为追求极致的办公室工作者而生1</text>
+			</view>
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
